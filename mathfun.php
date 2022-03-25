e7be246384d2e9273424a7199b26fdfcc0283556
bcgreen24 <bcgreen@gmail.com>
2022-03-25 08:18:27 -0500

    Initial commit.

---
+++ mathfun.php
@@ -0,0 +1,15 @@
+<?php
+
+trait {
+  public static function doubleIt(int $x) : int{
+        return $x*2;
+    }
+
+    public static function  halveIt(int $x) : int{
+        return $x/2;
+    }
+
+    public static function  squareIt(int $x) : int{
+        return $x^2;
+    }
+}
